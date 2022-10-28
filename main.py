03875862681c39cff75740a87bbad1b524bc147e
Svetlana Popova <115187745+saswomero@users.noreply.github.com>
2022-10-28 11:13:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from lab5.rectangle import *
+from lab5.circle import *
+
+col = Colour("purple")
+a = Rectangle(50, 40)
+print(a)
+
+print(' ')
+
+b = Circle(15)
+print(b)
+
+print (' ')
+
+c = Square(10)
+print(c)
+
+
+
+
+
+
